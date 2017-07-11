36311b20c3e48ab427af30bd1795bcab0e7c6649
IMDroidude <zareahmer89@gmail.com>
2017-07-12 01:20:23 +0500

    new commit

--- app/src/main/java/cleaner/asuper/droidudes/firebasetest/Splashy.java
+++ app/src/main/java/cleaner/asuper/droidudes/firebasetest/Splashy.java
@@ -3,8 +3,11 @@ package cleaner.asuper.droidudes.firebasetest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Splashy extends AppCompatActivity {
 
@@ -18,20 +21,24 @@ public class Splashy extends AppCompatActivity {
         DatabaseReference ref = database.child("rootNode");
         ref.setValue("TestRefValue");
 
-        /*DatabaseReference refWithUrl = FirebaseDatabase.getInstance()
+        DatabaseReference refWithUrl = FirebaseDatabase.getInstance()
                 .getReferenceFromUrl("https://<your-app>.firebaseio.com/path/to/data");
 
         refWithUrl.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    String x = snapshot.getKey() + snapshot.getValue();
+
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });*/
+        });
 
         // Get a reference to our posts
         /*final FirebaseDatabase database = FirebaseDatabase.getInstance();
